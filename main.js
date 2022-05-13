ced4c44656d19e385650a8cf97b25899b342ef45
Abhimanyu000 <89971842+Abhimanyu000@users.noreply.github.com>
2022-05-13 19:44:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -29,11 +29,30 @@ function my_keydown(e)
 	keyPressed = e.keyCode;
 	console.log(keyPressed);
 	
-		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+		
+	if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
 		{
 			aplhabetkey();
 			document.getElementById("d1").innerHTML="You pressed an Alphabet Key";
 			console.log("alphabet key");
+		}
+	else if(keyPressed >=48 && keyPressed<=57)
+		{
+			numberkey();
+			document.getElementById("d1").innerHTML="You pressed an Number Key";
+			console.log("number key");
+		}
+	else if(keyPressed >=37 && keyPressed<=40)
+		{
+			arrowkey();
+			document.getElementById("d1").innerHTML="You pressed an Arrow Key";
+			console.log("arrow key");
+		}
+	else if((keyPressed ==17) || (keyPressed==18) || (keyPressed==27))
+		{
+			specialkey();
+			document.getElementById("d1").innerHTML="You pressed an Special Key";
+			console.log("special key");
 		}
 	else{
 		otherkey();
@@ -47,14 +66,18 @@ function aplhabetkey(){
 }
 function numberkey()
 {
-	
+	img_image="numkey.png";
+	add();
 }
 function arrowkey()
 {
+	img_image="Arrkey.png";
+	add();
 }
 function specialkey()
 {
-	
+	img_image="spkey.png";
+	add();
 }
 function otherkey()
 {
